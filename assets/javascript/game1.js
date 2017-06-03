ab15dabddad1c10875fe5ff4f8321cc2ffb04bf3
Michael Dinh <mkdinh94@gmail.com>
2017-06-03 17:18:21 -0400

    adding replay ability

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -5,16 +5,22 @@
 // Game Parameter
 //////////////////////////////////////////////////////
 
-var life = 0;
+var kill = 0;
 var guessRemain = 10;
 var guessWrong = [];
-var word = [];
-var blank =[];
+var word =  [];
+var blank = [];
 var start = true;
+var stop = false;
+var next = false;
+var topic = "";
+var html = "";
+//Topics
+
 
 //Random indexing variables
-var allTopic = [predator1,predator2];
-var allBullet = ["one","two","three","four","five","six","seven","eight","nine","ten"]
+var allTopic = [predator1,predator2,TLAH,pass57,theyLive,dieHard,commando,dirtyHarry, hard2Kill,starWar2];
+var allBullet = ["one","two","three","four","five","six","seven","eight","nine","ten"];
 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -26,30 +32,36 @@ var allBullet = ["one","two","three","four","five","six","seven","eight","nine",
 //////////////////////////////////////////////////////
 
 function rand(myArray){
-	myArray[Math.floor(Math.random() * myArray.length)];
+	var element = myArray[Math.floor(Math.random() * myArray.length)];
+	return element;
 }
 
 //Check if a char is a letter
 //////////////////////////////////////////////////////
 
-function isLetter(str){
-	return str.length === 1 && str.match(/[a-z]/i);
+function isLetter(char){
+	return char.match(/[a-z]/i);
 }
 
+function reStart(){
+	var start = '<h1>The best kills are the one-liner kills!</h1>'
+     +'<p id="word">Press Enter to start playing!</p>';
+    document.querySelector("#game").innerHTML = reStart;
+
+}
 // Update #Game section
 //////////////////////////////////////////////////////
 
-function updateGame{
+function updateGame(){
 	var gameSect = "<b><h1>Movie:</b> " + topic.movie + "</h1>" +
 	'<p id="word">' + blank.join("") + '</p>';
-	
-	document.querySelector("#game").innerHTML = html;
+	document.querySelector("#game").innerHTML = gameSect;
 }
 
 // Update #Score section
 //////////////////////////////////////////////////////
 
-function updateScore{
+function updateScore(){
 	var score = "<h1><b>Score</b></h1>"
         + '<p class="lead">One-Liner Kill: ' + kill + "</p>"
         + '<p class="lead">Guess Remaining: ' + guessRemain + "<p>"
@@ -66,96 +78,114 @@ function updateScore{
 // While loop
 //////////////////////////////////////////////////////
 
-while(start === true){
-
 // Initializing Game
 //////////////////////////////////////////////////////
-
+//while(start === true){
 document.onkeyup = function(ev){ // On key press
 
 	var userGuess = ev.keyCode; // store key pressed into variable
-	var char = String.fromCharCode(userGuess).toLowerCase();
+	var code2Char = String.fromCharCode(userGuess);
 
-	//If key press is equal () start the game
-
-	if(userGuess === 13 || userGuess === 116){
-
-		//random picking topic
-		var topic = rand(allTopic);
-
-		// Hide characters of phrase
-
-		for(var i=0;i<topic.size();i++){
-			if(topic.phrase.charAt(i).match(/[a-z]/i)){
-				word.push(topic.phrase.charAt(i));
-				blank.push("_");
-			}else if(topic.phrase.charAt(i) === "!"){
-				word.push(topic.phrase.charAt(i));
-				blank.push(topic.phrase.charAt(i));
-			}else{
-				word.push(" ");
-				blank.push(" ");
-			}
-		}
-		// check the phrase in the topic and push each char in to an array
-		// at the same time, push a blank into the same index of another array
-		// if there is a space in the phrase, the index is empty
-
-
-		//Update #game html
-		updateGame();
-
-
-// Checking Key Pressed Against Char of a Phrase 
-//////////////////////////////////////////////////////
-
-	// Check for correct keys against characters array
-	for(i=0;i<topic.size();i++){
-			var char = word[i].toLowerCase();
-			if(String.fromCharCode(userGuess).toLowerCase() === char){	
-				blank[i] = word[i];
-				word[i]=word[i].toLowerCase();
-			}	
-		}
-	// If the key press is not equal to any of the char in the array
-	if(word.indexOf(String.fromCharCode(userGuess).toLowerCase()) === -1){
-			guessWrong.push(" "+String.fromCharCode(userGuess)); //push the key char into the already guessed array
-			guessRemain = guessRemain-1; // subtract 1 from the number of guess remained
-			
-			var bullet = rand(allBullet); // pick a random bullet id
-			document.getElementById(bullet).style.visibility = "initial"; // change the visiblity (hidden -> visible) 
-			allBullet.splice(allBullet.indexOf(bullet),1); // remove bullet id from array so it doesnt show up twice
-			document.getElementById('gun').play(); // play shot gun sound when ever a guess is wrong
-	}
-// If guessRemain === 0 (GAME OVER)
-//////////////////////////////////////////////////////
-
-	if(guessRemain === 0){
-		var correct = '<img src="assets/images/gameover.gif">'; //image for game over
-		updateGame(); // update #game id with image
-		blank=[]; //blank out array
-		word=[]; // blank out word array
-
-	}
-
-// If Guess all of the Char Correctly
-//////////////////////////////////////////////////////
-
-	if(blank.join("").toUpperCase() === word.join("").toUpperCase()){ // if blank array equal to word array
-		kill = kill +1; // increase kill var by 1
-		var correctImg = '<img src="'+topic.gif+'">'; // display images in object
-		document.querySelector("#game").innerHTML = correctImg; // update game with correct image
-		updateScore(); // update score
-		word=[]; // empty word array
-	}
-
-// Ask to quit
-//////////////////////////////////////////////////////
-
-		
+	if(userGuess === 116){
+			reStart();
 	
+	}
 
-		} // End of if loop
+	function play(userGuess){
+		//If key press is equal () start the game
+		if(userGuess === 13 ){	
+			if(start === false){return;}
+				blank= [];
+				word=[];
+				//random picking topic
+				topic = rand(allTopic);
+				topic.phrase = topic.phrase.toUpperCase();
 
-}// End of onkeyup funct.
-} // End of while loop
+				// Hide characters of phrase
+				// check the phrase in the topic and push each char in to an array
+				// at the same time, push a blank into the same index of another array
+				// if there is a space in the phrase, the index is empty
+				for(var i=0;i<topic.size();i++){
+					var phraseChar = topic.phrase.charAt(i);
+
+					if(isLetter(phraseChar)){
+						word.push(phraseChar);
+						blank.push("_");
+					}else if(phraseChar === "!" || phraseChar === "?" || phraseChar === "'"){
+						word.push(phraseChar);
+						blank.push(phraseChar);
+					}else{
+						word.push(" ");
+						blank.push(" ");
+					} // end of else loop
+
+				} // end of for loop
+
+			//Update #game html
+			updateGame();
+			start = false; // turn false, cannot press enter to start anymore
+		} // end of if(userGuess === 13) 
+
+		// Checking Key Pressed Against Char of a Phrase 
+		//////////////////////////////////////////////////////
+			
+		if(isLetter(code2Char) && userGuess !== 116 && userGuess && 13){
+			if(stop === true){return};
+			// Check for correct keys against characters array
+			for(i=0;i<topic.size();i++){
+				if(code2Char.toUpperCase() === word[i]){	
+					blank[i] = word[i];
+					word[i]=word[i];
+
+				} // end if loop
+			} // end for loop
+
+			// If the key press is not equal to any of the char in the array
+			if(word.indexOf(code2Char) === -1){
+					guessWrong.push(" "+String.fromCharCode(userGuess)); //push the key char into the already guessed array
+					guessRemain = guessRemain-1; // subtract 1 from the number of guess remained
+					
+					var bullet = rand(allBullet); // pick a random bullet id
+					document.getElementById(bullet).style.visibility = "initial"; // change the visiblity (hidden -> visible) 
+					allBullet.splice(allBullet.indexOf(bullet),1); // remove bullet id from array so it doesnt show up twice
+					document.getElementById('gun').play(); // play shot gun sound when ever a guess is wrong
+			}
+
+			updateGame(); // update #game id with image
+			updateScore(); // update score
+		}
+	} // END OF FUNCTION
+	play(userGuess);
+	// If guessRemain === 0 (GAME OVER)
+	//////////////////////////////////////////////////////
+
+		if(guessRemain === 0){
+			var gameover = '<img src="assets/images/gameover.gif">'; //image for game over
+			document.querySelector("#game").innerHTML = gameover; // update game with correct image	
+			//ask to play again
+			stop = true;
+		}
+
+	// If Guess all of the Char Correctly
+	//////////////////////////////////////////////////////
+
+
+		if(blank.join("").toUpperCase() === word.join("").toUpperCase()){ // if blank array equal to word array
+			kill = kill +1; // increase kill var by 1
+			var correctImg = '<img src="'+topic.gif+'">'; // display images in object
+			document.querySelector("#game").innerHTML = correctImg; // update game with correct image
+			next = true;
+			blank = [];
+		}
+
+	if(userGuess === 13 && next === true){
+				start = true;
+				updateGame();
+				updateScore();
+				play(userGuess)}
+				console.log(start);
+
+	// Ask to quit
+	//////////////////////////////////////////////////////
+	} // end of is Letter 
+//}// End of onkeyup funct.
---
+++ assets/javascript/topics.js
@@ -0,0 +1,102 @@
+var list = [predator1,predator2,TLAH,pass57,theyLive,dieHard,commando,dirtyHarry, hard2Kill,starWar2];
+
+var predator1 = {
+	movie: "Predator",
+	phrase: "Get to the Chopper!",
+	gif: "assets/images/predator1.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var predator2 = {
+	movie: "Predator",
+	phrase: "I aint got time to bleed",
+	gif: "assets/images/predator2.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var TLAH = {
+	movie: "The Last Action Hero",
+	phrase: "Hey, you wanna be a farmer?",
+	gif: "assets/images/TLAH.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+
+var pass57 = {
+	movie: "Passenger 57",
+	phrase: "Always bet on black",
+	gif: "assets/images/pass57.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var theyLive = {
+	movie: "They Live",
+	phrase: "I have come here to chew bubblegum and kick ass",
+	gif: "assets/images/theyLive.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var dieHard = {
+	movie: "Die Hard",
+	phrase: "Yippee ki yay!",
+	gif: "assets/images/dieHard.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var dirtyHarry = {
+	movie: "Dirty Harry",
+	phrase: "Do you feel lucky, punk?",
+	gif: "assets/images/dirtyHarry.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var commando = {
+	movie: "Commando",
+	phrase: "Let off some steam",
+	gif: "assets/images/commando.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var hard2Kill = {
+	movie: "Hard to Kill",
+	phrase: "I'm going to take you to the bank",
+	gif: "assets/images/hard2Kill.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
+
+var starWar2 = {
+	movie: "Star Wars: Empire Strike Back",
+	phrase: "I am your father!",
+	gif: "assets/images/sw.gif",
+	size: function propLen(){ 
+	var len = this.phrase.length;
+    return len;
+	},
+}
